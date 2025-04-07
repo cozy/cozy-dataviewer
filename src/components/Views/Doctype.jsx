91c546370aad4ead8e9d6e3e5a4cec0c73264d8f
doubleface <christophe@cozycloud.cc>
2025-04-07 16:13:13 +0200

    feat: enhance Doctype component with data fetching and loading state

--- src/components/Views/Doctype.jsx
+++ src/components/Views/Doctype.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+
+import { useClient, Q } from 'cozy-client'
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import SpinnerIcon from 'cozy-ui/transpiled/react/Icons/Spinner'
 
 export const Doctype = () => {
   const { doctype } = useParams()
 
+  const [data, setData] = useState()
+  const client = useClient()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await client.queryAll(Q(doctype))
+      setData(result)
+    }
+    fetchData()
+  }, [client, doctype])
+
+  if (data === undefined) {
+    return (
+      <div className="u-p-1">
+        <Icon icon={SpinnerIcon} spin />
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>Document Type: {doctype}</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   )
 }
